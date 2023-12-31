16c8b8bb311732b5d69079a3f23e8954f28e9a17
Atu77l <kesharwaniatul9935@gmail.com>
2023-12-31 14:48:27 +0530

    url of live server added in constant file

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,2 +1,5 @@
-export const CRYPTOCURRENCY_LIST='http://localhost:4000/api/top-cryptos'
-export const CONVERT_API='http://localhost:4000/api/convert'
+// export const CRYPTOCURRENCY_LIST='http://localhost:4000/api/top-cryptos'
+// export const CONVERT_API='http://localhost:4000/api/convert'
+
+export const CRYPTOCURRENCY_LIST='http://4.231.169.255:4000/api/top-cryptos'
+export const CONVERT_API='http://4.231.169.255:4000/api/convert'
